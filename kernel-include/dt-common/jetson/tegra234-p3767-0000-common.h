8cd785702998e37aa1a815509e5da28e7dbeb10f
Gautham Srinivasan <gauthams@nvidia.com>
2025-01-08 14:31:36 +0000

    t23x: overlay: add jetson-io support for ONX super
    Add compatible strings to support Orin NX/Nano super
    configuration in jetson-io tool.
    
    Bug 4131473
    
    Change-Id: Ia49e1cd5195203bd456dd3b21dd2173f1d44a6bf
    Reviewed-on: https://git-master.nvidia.com/r/c/device/hardware/nvidia/t23x-public-dts/+/3273805
    (cherry picked from commit eb7b23f3dc04177cf6b53c65b794d683a994afc8)
    Reviewed-on: https://git-master.nvidia.com/r/c/device/hardware/nvidia/soc/tegra/+/3300374
    Tested-by: Gautham Srinivasan <gauthams@nvidia.com>
    Reviewed-by: Amulya Yarlagadda <ayarlagadda@nvidia.com>
    GVS: buildbot_gerritrpt <buildbot_gerritrpt@nvidia.com>
    Reviewed-by: Yi-Wei Wang <yiweiw@nvidia.com>

--- kernel-include/dt-common/jetson/tegra234-p3767-0000-common.h
+++ kernel-include/dt-common/jetson/tegra234-p3767-0000-common.h
@@ -1,16 +1,6 @@
-/*
- * Copyright (c) 2023, NVIDIA CORPORATION & AFFILIATES.  All rights reserved.
+/* SPDX-License-Identifier: GPL-2.0-only
+ * SPDX-FileCopyrightText: Copyright (c) 2023-2025, NVIDIA CORPORATION & AFFILIATES.  All rights reserved.
  *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms and conditions of the GNU General Public License,
- * version 2, as published by the Free Software Foundation.
- *
- * This program is distributed in the hope it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
- * more details.
- */
-/*
  * Definitions for Jetson tegra234-p3767-0000 board.
  */
 
@@ -20,7 +10,12 @@
                                         "nvidia,p3768-0000+p3767-0001", \
                                         "nvidia,p3768-0000+p3767-0003", \
                                         "nvidia,p3768-0000+p3767-0004", \
-                                        "nvidia,p3768-0000+p3767-0005"
+                                        "nvidia,p3768-0000+p3767-0005", \
+                                        "nvidia,p3768-0000+p3767-0000-super", \
+                                        "nvidia,p3768-0000+p3767-0001-super", \
+                                        "nvidia,p3768-0000+p3767-0003-super", \
+                                        "nvidia,p3768-0000+p3767-0004-super", \
+                                        "nvidia,p3768-0000+p3767-0005-super"
 
 #define JETSON_COMPATIBLE_P3509		"nvidia,p3509-0000+p3767-0000", \
                                         "nvidia,p3509-0000+p3767-0001", \
